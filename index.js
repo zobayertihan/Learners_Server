316898c7989cb10ab94b2cc7669f487580567c02
zobayertihan <zobayertihan@gmail.com>
2023-01-08 02:32:05 +0600

    task done

--- index.js
+++ index.js
@@ -2,43 +2,41 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const courses = require('./data/courses.json');
-const course = require('./data/course.json');
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API running');
 })
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilc8iz8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/course', (req, res) => {
-    res.send(course);
-})
+async function run() {
+    try {
+        const subscribeCollection = client.db('Animator').collection('subscribers');
+        app.post('/subscribers', async (req, res) => {
+            const subscriber = req.body;
+            const result = await subscribeCollection.insertOne(subscriber);
+            if (result) {
+                res.send({
+                    success: true,
+                    message: 'Subscribed'
+                })
+            }
+        })
 
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourses = courses.find(n => n.id === id)
-    res.send(selectedCourses);
-})
+    }
+    finally {
 
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourse = course.find(n => n.id === id)
-    res.send(selectedCourse);
-})
+    }
+}
 
-
-// app.get('/courses/:id', (req, res) => {
-//     const id = req.params.id;
-//     const coursesCatagory = courses.filter(n => n.category_id === id)
-//     res.send(coursesCatagory);
-// })
+run().catch(e => console.error(e));
 
 app.listen(port, () => {
-    console.log('Learners running on port: ', port)
+    console.log('Server running on port: ', port)
 })
